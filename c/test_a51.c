bbce118f09b4ae6948f7305cbbd1ac5c9f3b847d
Lauren Kosub <laurenkosub@gmail.com>
2019-05-17 01:08:31 +0200

    added more options for n-bit lfsr and made test_a51 output encrypted msg

--- c/test_a51.c
+++ c/test_a51.c
@@ -1,8 +1,9 @@
 #include <machine/patmos.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-#define A51_ADDR 0xf00b0000
+#define A51_ADDR 0xf00c0000
 
 /*
  * Tests the A5/1 cipher stream protocol
@@ -11,14 +12,32 @@
  */
 int main() {
     volatile _IODEV int *io_ptr = (volatile _IODEV int *) A51_ADDR;
+    unsigned long val1, secKey = 0;
+    unsigned long plaintext = 0xbaddadd1;
     printf("Welcome to A5/1\n");
     printf("key is 0x1fabcd1f1fabcd1f\n");
-    int val1;
+    printf("plain text to encrypt is %lx\n", plaintext);
+    
+    /*
+    printf("enter a message to encrypt: ");
+    char line[1024];    
+    gets(line);
 
-    // wait for output to be printed ? ... 
-    printf("bitstream: ");
+    // turn plaintext to hex cipher text input
+    for (int j = 0; j < strlen(line) ; j++) {
+        plaintext = (plaintext | (strtoul(line[j], NULL, 16) << (4*j)));
+    }
+    */
+
+    //printf("114 bit secret key found via bit stream: ");
     for (int i = 0 ; i < 114; i++) {
         val1 = (*io_ptr) % 2; // get LSB
-        printf("%u", val1);
+        //printf("%u", val1);
+        secKey = secKey | (val1 << (113-i));
     }
+
+    // encrypt the message by XORing the message and the generated secret key
+    //printf("114 bit secret key printed in hex: %lx\n", secKey);
+    printf("encrypted message printed in hex: %lx\n", secKey ^ plaintext); 
+
 }
--- hardware/src/main/scala/io/NLFSR.scala
+++ hardware/src/main/scala/io/NLFSR.scala
@@ -30,19 +30,69 @@ class NLFSR(n : Int) extends Module() {
         // tap values determined via table @ 
         // https://www.embedded.com/print/4015086
         
-        when((n == 8).B) {
-            feedback := ~(res(1)^res(2)^res(3)^res(7))
+        when((n == 2).B) {
+            feedback := res(1) ^ res(0)
+	}.elsewhen((n == 3).B) {
+	    feedback := res(0)^res(2)
+        }.elsewhen((n == 4).B) {
+            feedback := res(0)^res(3)
+        }.elsewhen((n == 5).B) {
+            feedback := res(1)^res(4)
+        }.elsewhen((n == 6).B) {
+            feedback := res(0)^res(5)
+        }.elsewhen((n == 7).B) {
+            feedback := res(0)^res(6)
+        }.elsewhen((n == 8).B) {
+            feedback := res(1) ^ res(2) ^ res(3) ^ res(7)
+	}.elsewhen((n == 9).B) {
+	    feedback := res(3)^res(8)
+        }.elsewhen((n == 10).B) {
+            feedback := res(9)^res(2)
+	}.elsewhen((n == 11).B) {
+	    feedback := res(1)^res(10)
+        }.elsewhen((n == 12).B) {
+            feedback := res(0)^res(3)^res(5)^res(11)
+        }.elsewhen((n == 13).B) {
+            feedback := res(0)^res(2)^res(3)^res(12)
+        }.elsewhen((n == 14).B) {
+            feedback := res(0)^res(2)^res(4)^res(13)
+        }.elsewhen((n == 15).B) {
+            feedback := res(0)^res(14)
         }.elsewhen((n == 16).B) {
-            feedback := ~(res(1)^res(2)^res(4)^res(15))
+            feedback := res(1) ^ res(2) ^ res(4) ^ res(15)        
+	}.elsewhen((n == 17).B) {
+            feedback := res(2)^res(16)
+        }.elsewhen((n == 18).B) {
+            feedback := res(6)^res(17)
         }.elsewhen((n == 19).B) {
-            feedback := ~(res(0)^res(4)^res(1)^res(18))
-        }.elsewhen((n == 22).B) {
-            feedback := ~(res(0)^res(21))
+            feedback := res(0)^res(1)^res(4)^res(18)
+        }.elsewhen((n == 20).B) {
+            feedback := res(2) ^ res(19)
+	}.elsewhen((n == 21).B) {
+	    feedback := res(1)^res(20)
+	}.elsewhen((n == 22).B) {
+            feedback := res(0) ^ res(21)
         }.elsewhen((n == 23).B) {
-            feedback := ~(res(4)^res(22))
+            feedback := res(4) ^ res(22)
+        }.elsewhen((n == 24).B) {
+            feedback := res(0)^res(2)^res(3)^res(23)
+        }.elsewhen((n == 25).B) {
+            feedback := res(2)^res(24)
+        }.elsewhen((n == 26).B) {
+            feedback := res(1)^res(5) ^ res(25) ^ res(0)
+        }.elsewhen((n == 27).B) {
+            feedback := res(0) ^ res(1) ^ res(4) ^res(26)
+        }.elsewhen((n == 28).B) {
+            feedback := res(2)^res(27)
+        }.elsewhen((n == 29).B) {
+            feedback := res(1)^res(28)
+        }.elsewhen((n == 30).B) {
+            feedback := res(0)^res(3)^res(5)^res(29)
+        }.elsewhen((n == 31).B) {
+            feedback := res(2)^res(30)
         }.elsewhen((n == 32).B) {
-            feedback := ~(res(1) ^ res(2) ^ res(22))
-        }.otherwise { feedback := ~(res(1)^res(2)^res(4)^res(15)) }
+            feedback := res(1) ^ res(5) ^ res(6) ^ res(31)
+        }.otherwise { feedback := res(1)^res(2)^res(4)^res(15)  \\ default is 16 bit }
         
         val next_res = Cat(feedback,res(n-1, 1))
         res := next_res
